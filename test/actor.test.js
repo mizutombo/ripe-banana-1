6b3e31a145786668c589af499a3c458c2c6fecbc
mericks <mericks@users.noreply.github.com>
2017-02-13 10:04:29 -0800

    actor test passing but really fail (returning undefined)

--- test/actor.test.js
+++ test/actor.test.js
@@ -1,13 +1,14 @@
 const assert = require('chai').assert;
-const Actor = requuire('../lib/models/actor');
+const Actor = require('../lib/models/actor');
 
-Describe('Actor model', () => {
+describe('Actor model', () => {
     it('shows all actor fields', () => {
         return new Actor({
             name: 'Charlie Chaplin',
             dob: 1889-04-16
         }).validate()
-        .then(actor => console.log(actor));
+
+        .then(actor => console.log('actor is', actor));
         // .catch(err => { console.log(err); throw err; });
     })
 });
