164ed1e6ef3c8ad42837b3fa5aa065e7240facf7
mizutombo <thomas.shultz52@gmail.com>
2017-02-14 11:44:26 -0800

    fixes load-data.js

--- test/load-data.js
+++ test/load-data.js
@@ -6,17 +6,17 @@ const mongoose = require('mongoose');
 
 
 function loadData(collection) {
-	return `mongoimport --db ripe-banana-test --collection ${collection} --file data/${collection}.json --jsonArray `;
+    return `mongoimport --db ripe-banana-test --collection ${collection} --file data/${collection}.json --jsonArray `;
 }
 
 before(done => {
     mongoose.connection.dropDatabase();
 	childProcess.exec(loadData('films'), err => {
-		if(err) return done(err);
-		childProcess.exec(loadData('studios'), err => {
-			if(err) return done(err);
-			childProcess.exec(loadData('actors'), done);
-		});
-	});
+        if(err) return done(err);
+        childProcess.exec(loadData('studios'), err => {
+            if(err) return done(err);
+            childProcess.exec(loadData('actors'), done);
+        });
+    });
 });
 
