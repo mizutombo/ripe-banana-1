cbc4f7b4a657bd4e5dba46b6245f370b80b4e2b6
mizutombo <thomas.shultz52@gmail.com>
2017-02-14 03:51:34 -0800

    sets up order of tests in film-schema.test.js ... previous commit was actually to set up body parser function as module in body-parser.js

---
+++ test/film-schema.test.js
@@ -0,0 +1,23 @@
+const Film = require('../lib/models/film');
+const assert = require('chai').assert;
+
+describe('Film data schema :', () => {
+
+	it('checks for required "title" data', done => {
+
+
+	});
+
+	it('checks that "title" is a String', done => {
+
+	});
+
+	it('checks for required "released" data', done => {
+
+	});
+
+	it('checks that "released" is a Date', done => {
+
+	});	
+
+});
