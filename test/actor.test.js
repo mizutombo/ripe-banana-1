a4c08a393cd7bb07b09e64203cf7738a6496d4d3
mericks <mericks@users.noreply.github.com>
2017-02-14 13:36:35 -0800

    actor schema tests completed and passing

--- test/actor.test.js
+++ test/actor.test.js
@@ -1,13 +1,29 @@
 const assert = require('chai').assert;
 const Actor = require('../lib/models/actor');
 
-describe('Actor model', () => {
-    it('shows all actor fields', () => {
+describe('Actor model schema', () => {
+
+    it('accepts appropriately formated schema data', () => {
         return new Actor({
             name: 'Charlie Chaplin',
+            dob: '1889-04-16'
+        }).validate()
+        .catch(err => { console.log(err); throw err; });
+    })
+
+    it.skip('PASSING: rejects actor input w/out required field: name', () => {
+        return new Actor({
             dob: '1889-04-16'
         }).validate()
         .then(actor => console.log('actor=', actor))
         .catch(err => { console.log(err); throw err; });
     })
+
+    it('accepts actor input w/out non-required field: dob', () => {
+        return new Actor({
+            name: 'Charlie Chaplin',
+        }).validate()
+        .catch(err => { console.log(err); throw err; });
+    })
+
 });
