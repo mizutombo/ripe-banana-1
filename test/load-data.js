2f7d17d092a4f74c8093b967cb489531d8de588f
mericks <mericks@users.noreply.github.com>
2017-02-14 11:19:20 -0800

    corrected script to auto load data for testing

--- test/load-data.js
+++ test/load-data.js
@@ -1,16 +1,21 @@
 process.env.DB_URI = 'mongodb://localhost:27017/ripe-banana-test';
 require('../lib/connection');
+const childProcess = require('child_process');
 const mongoose = require('mongoose');
 
 
+
 function loadData(collection) {
-    return `mongoimport --db ripe-banana --collection ${collection} --file data/${collections}.json --jsonArray `;
+    return `mongoimport --db ripe-banana-test --collection ${collection} --file data/${collection}.json --jsonArray `;
 }
 
-before( () => {
+before(done => {
     mongoose.connection.dropDatabase();
-    loadData(films);
-    loadData(studios);
-    loadData(actors);
+    childProcess.exec(loadData('films'), err => {
+        if(err) return done(err);
+        childProcess.exec(loadData('studios'), err => {
+            if(err) return done(err);
+            childProcess.exec(loadData('actors'), done);
+        });
+    });
 });
-
