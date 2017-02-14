3f9254cdc67246c573db5b03cc8a4824f80f1df6
cmd-kvn <kvn.m.wong@gmail.com>
2017-02-14 10:13:04 -0800

    working on studio test about to merge from lab-kmt

--- test/studio.test.js
+++ test/studio.test.js
@@ -1,7 +1,32 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const request = require('superagent');
-const assert = chai.assert;
-const expect = chai.expect;
+const assert = require('chai').assert;
+const Studio = require('../lib/models/studio');
+// const mongoose = require('mongoose');
+// mongoose.Promise = Promise;
 
+describe.only('Studio model', () => {
+
+    function testHasValidationError(data) {
+        return new Studio(data)
+        .validate()
+        .then(
+            () => {throw new Error('validation should not succeed');},
+            () => {console.log('error is..', Error);}
+        );
+    }
+
+    it('shows studio name', () => {
+        return testHasValidationError({name: 1});
+
+        // return new Studio({
+        //     name: 'Warner Bros.',
+        //     address: {
+        //         city: 'Burbank',
+        //         state: 'California',
+        //         country: 'USA'
+        //     }
+        // }).validate()
+        // .then(studio => console.log('stdo=', studio))
+        // .catch(err => { console.log(err); throw err; });
+    });
+});
 
