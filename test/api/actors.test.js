8eaaad64e80f8e1957152889d9e261d5de0c1350
mericks <mericks@users.noreply.github.com>
2017-02-15 11:05:59 -0800

    working notest on actors routes and api tests

--- test/api/actors.test.js
+++ test/api/actors.test.js
@@ -1,1 +1,35 @@
-// holding file to test actors.js route
+const chai = require('chai');
+const assert = chai.assert;
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+const connection = require('../../lib/connection');
+const app = require('../../lib/app');
+
+const request = chai.request(app);
+
+
+describe('actors routes test', () => {
+
+
+    it('GET /actors returns an array of 11 actors', () => {
+        return request
+            .get('/actors')
+            .then (req => req.body)
+            .then(actors => assert.Equal(actors.length, 11))
+    });
+
+    it('GET /actors/:id returns a specific actor', () => {
+        return request
+            .get('/actors/58a368d61b83b3f1522a9fe3')
+            .then(req => req.body)
+            .then(actor => assert.deepEqual(actor,
+                 {
+                    "_id" : ObjectId("58a368d61b83b3f1522a9fe3"),
+                    "name" : "Robert Downey, Jr.",
+                    "dob" : "2000-01-01"
+                }
+             ))
+    });
+
+})
